3df4421c22572d0a265380e049ab20f947d3388f
laraludwig18 <laraludwig18@gmail.com>
2020-01-12 14:36:49 -0300

    Remove frontend folder

--- frontend/src/store/modules/rootSaga.js
+++
@@ -1,9 +0,0 @@
-import { all } from 'redux-saga/effects';
-
-import auth from './auth/sagas';
-import meetup from './meetup/sagas';
-import user from './user/sagas';
-
-export default function* rootSaga() {
-  return yield all([auth, meetup, user]);
-}
--- mobile/__tests__/helper/mocks.js
+++
@@ -1,4 +0,0 @@
-import MockAdapter from 'axios-mock-adapter';
-import api from '~/services/api';
-
-export const apiMock = new MockAdapter(api);
